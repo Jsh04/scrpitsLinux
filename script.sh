a05deb0f33e076ccea469e5995463b47d93d9a5f
Jose Silvio <josesilvio.bs@gmail.com>
2023-05-15 16:40:29 -0300

    Criando usuarios, pastas e grupos

--- script.sh
+++ script.sh
@@ -1,3 +1,27 @@
 #!bin/bash
 
+echo "Criando diretórios..."
+
+mkdir /publico 
+mkdir /adm
+mkdir /ven 
+mkdir /sec
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+useradd silvio -m -s ./bin/bash -p $(openssl passwd -cript senha123) -G GRP_ADM
+useradd roberto -m -s ./bin/bash -p $(openssl passwd -cript senha123) -G GRP_VEN
+useradd roberto -m -s ./bin/bash -p $(openssl passwd -cript senha123) -G GRP_SEC
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /ven
+chmod 770 /adm
+chmod 770 /sec
+
+
 
